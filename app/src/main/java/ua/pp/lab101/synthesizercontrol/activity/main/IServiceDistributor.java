67e957bedfd13fa065213d88da886d7e08dfeb46
ashram <ashram.astartes@gmailc.om>
2015-09-06 16:46:21 +0300

    Minor translation bugs and translation has been fixed.

--- app/src/main/java/ua/pp/lab101/synthesizercontrol/activity/main/IServiceDistributor.java
+++ app/src/main/java/ua/pp/lab101/synthesizercontrol/activity/main/IServiceDistributor.java
@@ -3,8 +3,8 @@ package ua.pp.lab101.synthesizercontrol.activity.main;
 import ua.pp.lab101.synthesizercontrol.service.BoardManagerService;
 
 /**
- * Created by ashram on 4/15/15.
+ * Created by Cherniakh Bohdan on 4/15/15.
  */
 public interface IServiceDistributor {
-    public BoardManagerService getService();
+    BoardManagerService getService();
 }
